e382567aafec8f7050d77d9d0c42319a03e3ffd5
kamal.berrami <mac@macs-mbp.home>
2018-10-09 22:25:39 +0100

    mise en place du designPattern Observer

---
+++ designPattern/src/observer/Company.java
@@ -0,0 +1,16 @@
+package observer;
+
+public class Company  implements Observer {
+
+    private String name;
+    private String address;
+
+    public Company(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public void update(String message) {
+        System.out.printf("Company %s Got new notification: %s%n", this.name, message);
+    }}
---
+++ designPattern/src/observer/Subject.java
@@ -0,0 +1,22 @@
+package observer;
+
+public interface Subject {
+	
+    /**
+     * Add Observer Method
+     * @param observer observer
+     */
+    void addObserver(Observer observer);
+
+    /**
+     * Remove an observer method
+     * @param observer
+     */
+    void removeObserver(Observer observer);
+
+    /**
+     * Notify all observers of this subject
+     */
+    void notifyAllObservers();
+
+}
